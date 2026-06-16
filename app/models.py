714c1241a866e3414ab255337b1b12cc8b4d2b0c
thedza49 <petta.property@gmail.com>
2026-06-15 20:18:09 -0700

    Update models.py

--- app/models.py
+++ app/models.py
@@ -33,3 +33,19 @@ class Rider(Base):
 engine = create_engine("sqlite:///./letour.db", connect_args={"check_same_thread": False})
 Base.metadata.create_all(bind=engine)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# --- Add this to the end of app/models.py ---
+def seed_data():
+    db = SessionLocal()
+    if not db.query(Rider).first():
+        sample_riders = [
+            Rider(name="Tadej Pogacar", team="UAE", price=55.0, uci_rank=1),
+            Rider(name="Jonas Vingegaard", team="Visma", price=52.0, uci_rank=2),
+            Rider(name="Remco Evenepoel", team="Soudal", price=48.0, uci_rank=3)
+        ]
+        db.add_all(sample_riders)
+        db.commit()
+    db.close()
+
+# Call this once
+seed_data()
